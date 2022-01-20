cb09c5ff5b323b7574b2a52fe740e1253733fe86
StevenSDove <steven.dove@gmail.com>
2022-01-20 15:12:38 -0500

    2 files modified, 3 added

--- src/app/contactmanager/contactmanager.module.ts
+++ src/app/contactmanager/contactmanager.module.ts
@@ -12,6 +12,7 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { UserService } from './services/user.service';
 import { HttpClientModule } from '@angular/common/http';
+import { NotesComponent } from './components/notes/notes.component';
 
 const routes: Routes = [
   {
@@ -38,6 +39,7 @@ const routes: Routes = [
     ContactManagerAppComponent,
     ToolbarComponent,
     SidenavComponent,
+    NotesComponent,
     MainContentComponent,
   ],
   providers: [UserService],
