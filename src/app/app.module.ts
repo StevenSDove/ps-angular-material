4ab3cb47361e0d4bcafc493557e096a1d8c4c77c
StevenSDove <steven.dove@gmail.com>
2022-01-20 13:01:43 -0500

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -24,6 +25,7 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     RouterModule.forRoot(routes),
   ],
   declarations: [AppComponent],
--- src/app/contactmanager/contactmanager-app.component.ts
+++ src/app/contactmanager/contactmanager-app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact-manager',
@@ -6,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contactmanager-app.component.css'],
 })
 export class ContactManagerAppComponent implements OnInit {
-  constructor() {}
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIconSet(
+      sanitizer.bypassSecurityTrustResourceUrl(
+        'https://stackblitz.com/files/sdove-ps-angular-material/github/StevenSDove/ps-angular-material/master/src/asets/avatars.svg'
+      )
+    );
+  }
 
   ngOnInit() {}
 }
