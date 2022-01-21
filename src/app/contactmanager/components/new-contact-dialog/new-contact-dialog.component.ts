4433fc5f487d6a1cd224090c68a37db0979e9bbe
StevenSDove <steven.dove@gmail.com>
2022-01-21 13:10:12 -0500

    4 files modified

--- src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
+++ src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-new-contact-dialog',
@@ -10,17 +11,32 @@ import { User } from '../../models/user';
 })
 export class NewContactDialogComponent implements OnInit {
   user: User;
+  saving: boolean = false;
   avatars = ['svg-1', 'svg-2', 'svg-3', 'svg-4'];
   name = new FormControl('', [Validators.required]);
+  errorMessage: string = '';
 
-  constructor(private dialogRef: MatDialogRef<NewContactDialogComponent>) {}
+  constructor(
+    private dialogRef: MatDialogRef<NewContactDialogComponent>,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
     this.user = new User();
   }
 
   save(): void {
-    this.dialogRef.close(this.user);
+    this.user.name = this.name.value;
+    this.errorMessage = '';
+    this.saving = true;
+    this.userService.addUser(this.user).subscribe({
+      next: (newUser) => this.dialogRef.close(newUser),
+      error: (error) => {
+        this.errorMessage = error;
+        console.error('Could not add user.', error, this.user);
+        this.saving = false;
+      },
+    });
   }
 
   dismiss(): void {
--- src/app/contactmanager/services/user.service.ts
+++ src/app/contactmanager/services/user.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { BehaviorSubject, Observable, of, Subscription } from 'rxjs';
 import { User } from '../models/user';
 
 @Injectable()
@@ -19,14 +19,17 @@ export class UserService {
     return this._users.asObservable();
   }
 
+  notifyUsersUpdated(): void {
+    this._users.next(Object.assign({}, this.dataStore).users);
+  }
+
   loadAll(): Subscription {
     const usersUrl = 'https://angular-material-api.azurewebsites.net/users';
 
     return this.http.get<User[]>(usersUrl).subscribe({
       next: (data) => {
         this.dataStore.users = data;
-        // I do not think that this does what the instructor thinks that it does...
-        this._users.next(Object.assign({}, this.dataStore).users);
+        this.notifyUsersUpdated();
       },
       error: (err) => console.log('Failed to fetch users', err),
     });
@@ -34,5 +37,22 @@ export class UserService {
 
   userById(id: number): User {
     return this.dataStore.users.find((x) => x.id == id);
+  }
+
+  addUser(user: User): Observable<User> {
+    return new Observable<User>((observer) => {
+      // timeout to see what it is doing on a delay :-)
+      setTimeout(() => {
+        if (!user.name.includes(' ')) {
+          observer.error('Must include first and last name');
+          return;
+        }
+        const maxId = this.dataStore.users.length; // this.dataStore.users.reduce((max,cur) => cur.id > max.id ? cur : max).id;
+        user.id = maxId + 1;
+        this.dataStore.users.push(user);
+        this.notifyUsersUpdated();
+        observer.next(user);
+      }, 1000);
+    });
   }
 }
